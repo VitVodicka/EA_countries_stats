63c95395ecaf09de44ede74083b426baf8b96cb2
Vít <xvodick6@mendelu.cz>
2024-05-21 22:06:32 +0200

    init

---
+++ EA_countries_stats/src/main/java/com/example/ea_countries_stats/EaCountriesStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ea_countries_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaCountriesStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaCountriesStatsApplication.class, args);
+    }
+
+}
---
+++ EA_countries_stats/src/test/java/com/example/ea_countries_stats/EaCountriesStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ea_countries_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaCountriesStatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
